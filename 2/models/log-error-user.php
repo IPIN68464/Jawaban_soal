b8f939382e20f69c9f1909b583f2c7e854aa68f6
Ipin <134088043+IPIN68464@users.noreply.github.com>
2023-11-14 21:43:35 +0800

    Delete 2 directory

--- 2/models/log-error-user.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host='localhost';
-$user='Website';
-$pass='4r1f1n';
-$db='karyawan';
-$line="insert into I_ERROR_APPLICATION values(null,'$pid','$messageError','error','$DBjenis','$pid',NOW());";
-$conn=mysqli_connect($host, $user, $pass, $db);
-$sql=mysqli_query($conn, $line);
-?>
